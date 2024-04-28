cdfa69d1bf639e55024ee3ac40b39e28803722bb
carvalhoJao <pedrocarvalho2021@hotmail.com>
2024-04-28 19:34:16 -0300

    Função de email

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ ready(() => {
 })
 
 // Insira aqui sua função de Alert ao clicar no botão Enviar
+
+$(document).ready( function() {
+    console.log("teste");
+    $(".enviar").click(function() {
+        var inputNome = document.getElementById("name");
+        var inputEmail = document.getElementById("email");
+        var inputMensagem = document.getElementById("mensagem");
+        var textoNome = inputNome.value;
+        var textoEmail = inputEmail.value;
+        var textoMensagem = inputMensagem.value;
+
+        if (textoNome === "" || textoEmail === "") {
+            alert("Você precisa preencher seu nome e email.");
+        } else if (textoMensagem === "") {
+            alert("Sua mensagem não pode estar vazia");
+        } else {
+            alert("Email enviado com sucesso!");
+        }
+        // alert("Botão clicado");
+    })
+});
